a666b91b2c84454635d908f03ec780ff05d22b5e
amanda <adiner2@illinois.edu>
2019-04-27 14:27:39 -0500

    Timer

--- app/src/main/java/com/example/thechallen_ge/Tasks.java
+++ app/src/main/java/com/example/thechallen_ge/Tasks.java
@@ -1,17 +1,81 @@
 package com.example.thechallen_ge;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class Tasks extends AppCompatActivity {
+    private TextView countDownText;
+    private Button countdownButton;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMilliseconds = 30000; //30sec?
+    private boolean timerRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
 
+        countDownText = findViewById(R.id.countdown_text);
+        startStop();
+        //countdownButton = findViewById(R.id.countdown_button);
+
+/*          ((View) countdownButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+          public void onClick(View view) {
+                s
+            }
+        });
+*/
         findViewById(R.id.takeQuiz).setOnClickListener(v -> completeTask());
+    }
+
+    public void startStop() {
+        if (timerRunning) {
+            stopTimer();
+        } else {
+            startTimer();
+        }
+    }
+
+    public void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeftInMilliseconds = l;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+        timerRunning = true;
+    }
+
+    public void stopTimer() {
+        countDownTimer.cancel();
+        timerRunning = false;
+
+    }
+
+    public void updateTimer() {
+        int seconds = (int) timeLeftInMilliseconds / 1000;
+
+        String timeLeftText;
+        timeLeftText = "0:" + seconds;
+        if (seconds < 10) {
+            timeLeftText += "0";
+            timeLeftText += seconds;
+
+            countDownText.setText(timeLeftText);
+        }
     }
 
     void completeTask() {
